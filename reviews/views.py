d3cd0c5d72508d7e6b538e43a7c69814784e942d
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-30 15:08:59 +0300

    Updated views.py.
    Created new view for book_search functions that renders the new html page.

--- reviews/views.py
+++ reviews/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     name = "world"
     return render(request, "base.html", {"name": name})
+
+def book_search(request):
+    search_item = request.GET.get("search", "")
+    return render(request, "search_results.html", {"search_item": search_item})
