afa57a858e4940ab62f6e7436f7b2ed74d94dd24
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-31 18:40:53 +0300

    Updated reviews.py.
    Added Book class model.

--- reviews/models.py
+++ reviews/models.py
@@ -12,3 +12,16 @@ class Puplisher(models.Model):
     email = models.EmailField(
         help_text="The publisher's contact email address."
     )
+
+class Book(models.Model):
+    """Represent the table book in db, info about published book"""
+    title = models.CharField(
+        max_length=70, help_text="The title of the book."
+    )
+    publication_date = models.DateField(
+        verbose_name="Date the book was published."
+    )
+    isbn = models.CharField(
+        max_length=20, verbose_name="ISBN number of the book."
+    )
+    
