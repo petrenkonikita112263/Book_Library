1ce1ebfb6e4cf71f5ac3ca6535ea1a6b27d63466
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-08-31 17:48:49 +0300

    Updated models.py.
    Created new model Publisher.

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Puplisher(models.Model):
+    """Represents the table publisher in db, info about a company
+    that publishes books."""
+    name = models.CharField(
+        max_length=50, help_text="The name of the publisher."
+    )
+    website = models.URLField(
+        help_text="The publisher's website link."
+    )
+    email = models.EmailField(
+        help_text="The publisher's contact email address."
+    )
