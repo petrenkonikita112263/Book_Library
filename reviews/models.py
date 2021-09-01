7c9f368efff0e915d6b87b490eb9204b8e3ee41a
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-01 12:32:31 +0300

    Updated models.py.
    Added Review model with some fields and foreign keys to other models.

--- reviews/models.py
+++ reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib import auth
 
 
 class Puplisher(models.Model):
@@ -38,4 +39,26 @@ class Contributor(models.Model):
     )
     email = models.EmailField(
         help_text="The contact email for the contributor."
+    )
+
+
+class Review(models.Model):
+    """A information about user-provided review comments and ratings for books."""
+    content = models.TextField(
+        help_text="The Review text."
+    )
+    rating = models.IntegerField(
+        help_text="The rating the reviewer has given."
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True, help_text="The date and time the review was created."
+    )
+    date_edited = models.DateTimeField(
+        null=True, help_text="The date and time the review was last edited."
+    )
+    creator = models.ForeignKey(
+        auth.get_user_model(), on_delete=models.CASCADE
+    )
+    book = models.ForeignKey(
+        Book, on_delete=models.CASCADE, help_text="The Book that this review is for."
     )
