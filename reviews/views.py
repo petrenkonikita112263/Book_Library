5d40f91fba572c8dc3fb1c67009906238be7346c
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-03 20:58:01 +0300

    Updated views.py.
    Created new function-based view - welcome view, that displays the number of books from DB.

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
 
 
 def index(request):
     name = "world"
     return render(request, "base.html", {"name": name})
 
+
 def book_search(request):
     search_item = request.GET.get("search", "")
     return render(request, "search_results.html", {"search_item": search_item})
+
+
+def welcome_view(request):
+    message = f"<html><h1>Welcome to Bookr application!</h1>" \
+              f"<p>{Book.objects.count()} books and counting!</p></html>"
+    return HttpResponse(message)
