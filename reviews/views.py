ed54ff2a6337b14194e49cd8627d4c7415769620
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-04 12:00:46 +0300

    Updated reviews/views.py.
    Created new view method, that queries the list of all books, for each book on it calculates
    the average rating and number of reviews posted, and saves it into list books_with_reviews
    as list of dictionaries.

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-from .models import Book
+from .models import Book, Review
+from .utils import average_rating
 
 
 def index(request):
@@ -16,3 +17,29 @@ def book_search(request):
 def welcome_view(request):
     books_quantity = Book.objects.count()
     return render(request, "base.html", {"books_quantity": books_quantity})
+
+
+def book_list(request):
+    books = Book.objects.all()                                      # query all books from table
+    books_with_reviews = []
+    for book in books:
+        reviews = book.review_set.all()                             # get all reviews for each book
+        if reviews:                                                 # if there're any reviews
+            book_rating = average_rating(
+                [review.rating for review in reviews]               # list comprehension
+            )
+            number_of_reviews = len(reviews)
+        else:
+            book_rating = None
+            number_of_reviews = 0
+        books_with_reviews.append(                                  # add a list of dictionaries
+            {
+                "book": book,
+                "book_rating": book_rating,
+                "number_of_reviews": number_of_reviews
+            }
+        )
+    context = {
+        "books_with_reviews": books_with_reviews
+    }
+    return render(request, "reviews/books_list.html", context)
