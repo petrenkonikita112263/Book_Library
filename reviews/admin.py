2505ae48363e5ac1826024bffeaac2bc774ec1bf
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-05 18:16:42 +0300

    Updated admin.py.
    Created own class from AdminSite with customize attributes.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,10 +1,20 @@
-from django.contrib import admin
+from django.contrib.admin import AdminSite
 from reviews.models import Publisher, Book, Contributor, \
     BookContributor, Review
 
+
+class BookrAdminSite(AdminSite):
+    """Custom admin site for Bookr application."""
+    title_header = "Bookr Admin"
+    site_header = "Bookr administration"
+    index_title = "Book site admin"
+
+
+admin_site = BookrAdminSite(name="bookr")
+
 """Registrate models for AdminSite."""
-admin.site.register(Publisher)
-admin.site.register(Book)
-admin.site.register(Contributor)
-admin.site.register(BookContributor)
-admin.site.register(Review)
+admin_site.register(Publisher)
+admin_site.register(Book)
+admin_site.register(Contributor)
+admin_site.register(BookContributor)
+admin_site.register(Review)
