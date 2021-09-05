de047e9db66efbb02476f37a56f5cb96f463e70c
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-05 16:00:34 +0300

    Updated admin.py.
    Makes the models be available in admin page.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from reviews.models import Publisher, Book, Contributor, \
+    BookContributor, Review
 
-# Register your models here.
+"""Registrate models for AdminSite."""
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Contributor)
+admin.site.register(BookContributor)
+admin.site.register(Review)
