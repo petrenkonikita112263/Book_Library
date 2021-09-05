5584b11c0df6c3d7a6c7f31036be49681a5e5c2b
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-05 18:45:26 +0300

    Updated admin_apps.py.
    Before renaming, added one class ReviewsAdminConfig that sets the default
    site from BookrAdminSite class.

--- reviews/admin_apps.py
+++ reviews/admin_apps.py
@@ -6,5 +6,6 @@ class ReviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'reviews'
 
+
 class ReviewsAdminConfig(AdminConfig):
     default_site = "admin.BookrAdminSite"
