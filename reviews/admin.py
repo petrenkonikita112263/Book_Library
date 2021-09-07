9e8aa3d25e0dcacace9ec2076bc54448c62c95a1
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-07 19:16:16 +0300

    Updated reviews/admin.py.
    Register ContributorAdmin to model Contributor.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from admin import BookAdmin, ReviewAdmin
+from admin import BookAdmin, ReviewAdmin, ContributorAdmin
 from reviews.models import Publisher, Book, Contributor, \
     BookContributor, Review
 
 """Registrate models for AdminSite."""
 admin.site.register(Publisher)
 admin.site.register(Book, BookAdmin)
-admin.site.register(Contributor)
+admin.site.register(Contributor, ContributorAdmin)
 admin.site.register(BookContributor)
 admin.site.register(Review, ReviewAdmin)
