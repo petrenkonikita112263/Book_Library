cd9741c0502a3986b245ada3fed0f08997ae89bc
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-07 14:16:50 +0300

    Rollback.
    Rollback to previous commit de047e9.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,12 +1,10 @@
-from admin import BookrAdminSite
+from django.contrib import admin
 from reviews.models import Publisher, Book, Contributor, \
     BookContributor, Review
 
-admin_site = BookrAdminSite(name="bookr")
-
 """Registrate models for AdminSite."""
-admin_site.register(Publisher)
-admin_site.register(Book)
-admin_site.register(Contributor)
-admin_site.register(BookContributor)
-admin_site.register(Review)
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Contributor)
+admin.site.register(BookContributor)
+admin.site.register(Review)
