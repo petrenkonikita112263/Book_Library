24df37ec0603950055a4a75c373052fe116ef049
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-07 15:48:36 +0300

    Updated reviews/admin.py.
    Registrate ReviewAdmin class to Review model.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from admin import BookAdmin
+from admin import BookAdmin, ReviewAdmin
 from reviews.models import Publisher, Book, Contributor, \
     BookContributor, Review
 
@@ -8,4 +8,4 @@ admin.site.register(Publisher)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Contributor)
 admin.site.register(BookContributor)
-admin.site.register(Review)
+admin.site.register(Review, ReviewAdmin)
