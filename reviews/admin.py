73c340b8687617d8303282806271981895092bc6
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-07 14:21:10 +0300

    Updated reviews/admin.py.
    Registrate BookAdmin class to Book model.

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from admin import BookAdmin
 from reviews.models import Publisher, Book, Contributor, \
     BookContributor, Review
 
 """Registrate models for AdminSite."""
 admin.site.register(Publisher)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
 admin.site.register(Contributor)
 admin.site.register(BookContributor)
 admin.site.register(Review)
