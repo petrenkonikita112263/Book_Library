ebc8d650e3bade99e7d3118774fe084a24014af8
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-20 15:56:09 +0300

    Updated reviews/urls.py.
    Added two new url maps for editing and creating the publisher.

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.welcome_view, name="welcome_view"),
     path("books/", views.book_list, name="book_list"),
-    path("books/<int:book_id>", views.get_book_detail, name="get_book_detail")
+    path("books/<int:book_id>", views.get_book_detail, name="get_book_detail"),
+    path("publisher/<int:publisher_id>", views.publisher_edit, name="publisher_edit"),
+    path("publisher/new/", views.publisher_edit, name="publisher_create")
 ]
