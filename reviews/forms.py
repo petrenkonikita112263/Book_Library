4752b2496e2dcca8fa64c9427849b9f69be94b3f
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-09-21 16:49:06 +0300

    Updated reviews/forms.py.
    Created and filled ReviewForm class for Review model.

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Publisher
+from .models import Publisher, Review
 
 SEARCH_CHOICES = (
     ("title", "Title"),
@@ -20,3 +20,14 @@ class PublisherForm(forms.ModelForm):
     class Meta:
         model = Publisher
         fields = "__all__"
+
+
+class ReviewForm(forms.ModelForm):
+    """Creates the review form based on Review model, excludes two fields
+    and overrides rating field."""
+
+    class Meta:
+        model = Review
+        exclude = ["date_edited", "book"]
+
+    rating = forms.IntegerField(min_value=0, max_value=5)
