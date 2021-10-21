a80cd97fa338231d4a49d26bc9bfda32e02c3b7d
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-21 13:01:46 +0300

    Registrate User model.

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import User
 
 
 class BookrAdmin(admin.AdminSite):
@@ -6,3 +7,4 @@ class BookrAdmin(admin.AdminSite):
 
 
 admin_site = BookrAdmin(name="bookr_admin")
+admin_site.register(User)
