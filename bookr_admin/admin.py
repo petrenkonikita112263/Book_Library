46979e7c74876e80b1d28557020fbdfc5a147c4e
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-21 13:33:59 +0300

    Remove the import statement, instance and register.

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,10 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import User
 
 
 class BookrAdmin(admin.AdminSite):
     site_header = "Bookr administration"
-
-
-admin_site = BookrAdmin(name="bookr_admin")
-admin_site.register(User)
