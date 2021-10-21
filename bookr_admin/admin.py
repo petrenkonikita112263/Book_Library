81e8f02271206127a81fae37f0a1e74126263da2
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-21 16:06:33 +0300

    Add profile view function in BookrAdmin class.

--- bookr_admin/admin.py
+++ bookr_admin/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.template.response import TemplateResponse
+from django.urls import path
 
 
 class BookrAdmin(admin.AdminSite):
     site_header = "Bookr administration"
     logout_template = "admin/logout.html"
+
+    def profile_view(self, request):
+        request.current_app = self.name
+        context = self.each_context(request)
+        return TemplateResponse(
+            request, "admin/admin_profile.html", context
+        )
