1df86e816e6cd0993c4c383b3528dfb4a25f5e66
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-21 16:55:17 +0300

    Remove unused class.

--- admin.py
+++ admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
 
 
-class BookrAdminSite(admin.AdminSite):
-    """Custom admin site for Bookr application."""
-    title_header = "Bookr Admin"
-    site_header = "Bookr administration"
-    index_title = "Book site admin"
-
-
 class BookAdmin(admin.ModelAdmin):
     """Customise the book displaying in admin site."""
     list_display = ("title", "isbn")
