d8c590a3f09b3e9a30862575fbb69266e0a7742f
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-21 13:44:27 +0300

    Use AdminConfig to overrides the default_site property.

--- bookr_admin/apps.py
+++ bookr_admin/apps.py
@@ -1,6 +1,5 @@
-from django.apps import AppConfig
+from django.contrib.admin.apps import AdminConfig
 
 
-class BookrAdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'bookr_admin'
+class BookrAdminConfig(AdminConfig):
+    default_site = "bookr_admin.admin.BookrAdmin"
