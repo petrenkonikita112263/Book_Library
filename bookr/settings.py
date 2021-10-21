782e2affbb0f0e145489979289279a422329a5b2
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-21 13:45:18 +0300

    Replace path to AdminConfig class from python file to bookr_admin app.

--- bookr/settings.py
+++ bookr/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.ReviewsAdminConfig',
+    'bookr_admin.apps.BookrAdminConfig',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
