5fce5f6e55d959fce6262e3d91baaaef244b444b
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-26 18:04:42 +0300

    Add filter_demo application to project URL.

--- bookr/urls.py
+++ bookr/urls.py
@@ -28,7 +28,8 @@ urlpatterns = [
     path('accounts/profile/', bookr.views.profile, name='profile'),
     path('admin/', admin.site.urls),
     path("", include("reviews.urls")),
-    path("book_search/", reviews.views.book_search, name="book_search")
+    path("book_search/", reviews.views.book_search, name="book_search"),
+    path("filter_demo/", include("filter_demo.urls"))
 ]
 
 if settings.DEBUG:
