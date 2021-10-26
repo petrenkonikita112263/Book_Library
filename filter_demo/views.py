281d818f961b1337ea329bbe384de4c52a739f2a
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-26 18:27:00 +0300

    Add new view based function greeting_view

--- filter_demo/views.py
+++ filter_demo/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def index(request):
     names = "john,doe,mark,swain"
     return render(request, "filter_demo/index.html", {"names": names})
+
+
+def greeting_view(request):
+    return render(request, "filter_demo/simple_tag_template.html", {"username": "jdoe"})
