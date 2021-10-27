39534f461dc06f5b0dbb6b954e2eb421ff71a356
Nikita Petrenko <petrenkonikserg@gmail.com>
2021-10-27 16:57:23 +0300

    Add new urls mapping for CRUD operations.

--- book_management/urls.py
+++ book_management/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import BookRecordFormView, FormSuccessView
+from .views import BookRecordFormView, FormSuccessView, BookUpdateView, \
+    BookDeleteView, DeleteSuccessView, BookRecordDetailView
 
 urlpatterns = [path("new_book_record", BookRecordFormView.as_view(), name="book_record_form"),
-               path("entry_success", FormSuccessView.as_view(), name="form_success")]
+               path("entry_success", FormSuccessView.as_view(), name="form_success"),
+               path("book_record_update/<int:pk>", BookUpdateView.as_view(), name="book_update"),
+               path("book_record_delete/<int:pk>", BookDeleteView.as_view(), name="book_delete"),
+               path("delete_success", DeleteSuccessView.as_view(), name="delete_success"),
+               path("book_record_detail/<int:pk>", BookRecordDetailView.as_view(), name="book_detail")]
