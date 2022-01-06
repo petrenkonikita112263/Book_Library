35c9773700f41c517606eb1bd03940a1d053cef1
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-06 18:31:11 +0200

    Create ContributorView class.

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -1,8 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import generics
-from .models import Book
-from .serializers import BookSerializer
+from .models import Book, Contributor
+from .serializers import BookSerializer, ContributorSerializer
 
 
 @api_view(http_method_names=["GET"])
@@ -14,3 +14,8 @@ def first_api_view(request):
 class AllBooks(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class ContributorView(generics.ListAPIView):
+    queryset = Contributor.objects.all()
+    serializer_class = ContributorSerializer
