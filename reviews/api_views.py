231bcbf8b0b7bac208855d63b4769b4fc02c9b41
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-06 17:03:46 +0200

    Add allowed method to decorator.

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from .models import Book
 
 
-@api_view
+@api_view(http_method_names=["GET"])
 def first_api_view(request):
     number_books = Book.objects.count()
     return Response({"number_books": number_books})
