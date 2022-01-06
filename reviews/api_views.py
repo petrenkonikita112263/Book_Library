94ee785605aaacfc2ad4607fdbd456ff9f6449d4
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-06 16:56:16 +0200

    Add first_view_api decorator functions.

---
+++ reviews/api_views.py
@@ -0,0 +1,9 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Book
+
+
+@api_view
+def first_api_view(request):
+    number_books = Book.objects.count()
+    return Response({"number_books": number_books})
