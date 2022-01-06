fff1f5818ea5274f67d3bdc2ba126aa83f3210b2
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-06 18:04:39 +0200

    Replace view api functions to class based.

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,14 +1,14 @@
 from rest_framework import serializers
+from .models import Book, Publisher
 
 
 class PublisherSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    website = serializers.URLField()
-    email = serializers.EmailField()
+    class Meta:
+        model = Publisher
+        fields = ["name", "website", "email"]
 
 
 class BookSerializer(serializers.Serializer):
-    title = serializers.CharField()
-    publication_date = serializers.DateField()
-    isbn = serializers.CharField()
-    publisher = PublisherSerializer()
+    class Meta:
+        model = Book
+        fields = ["title", "publication_date", "isbn", "publisher"]
