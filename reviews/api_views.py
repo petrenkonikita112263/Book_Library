95820e9c7e136e0466a9d1b1b52558601f5a989e
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-06 17:48:46 +0200

    Create new api decorator function to get all books.

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -1,9 +1,17 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Book
+from .serializers import BookSerializer
 
 
 @api_view(http_method_names=["GET"])
 def first_api_view(request):
     number_books = Book.objects.count()
     return Response({"number_books": number_books})
+
+
+@api_view(http_method_names=["GET"])
+def all_books(request):
+    books = Book.objects.all()
+    book_serializer = BookSerializer(books, many=True)
+    return Response(book_serializer.data)
