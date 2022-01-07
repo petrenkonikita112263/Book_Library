32fce165f7bf1d45d40ae116c5c42a66d2915ec2
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-07 19:38:02 +0200

    Create Login class that creates token for user.

--- reviews/api_views.py
+++ reviews/api_views.py
@@ -1,6 +1,12 @@
+from django.contrib.auth import authenticate
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework.views import APIView
 from rest_framework import generics, viewsets
 from .models import Book, Contributor, Review
 from .serializers import BookSerializer, ContributorSerializer, ReviewSerializer
@@ -27,3 +33,13 @@ class ReviewViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = ReviewSerializer
     pagination_class = LimitOffsetPagination
     authentication_classes = []
+
+
+class Login(APIView):
+
+    def post(self, request):
+        user = authenticate(username=request.data.get("username"), password=request.data.get("password"))
+        if not user:
+            return Response({"error": "Credentials are incorrect or user does not exist"}, status=HTTP_404_NOT_FOUND)
+        token, _ = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key}, status=HTTP_200_OK)
