cacec307711c386f921dec1580f7ba1b1a751002
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-01-07 19:24:14 +0200

    Register the new created BookViewSet and ReviewViewSet, add it as new URL mapping.

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,8 +1,12 @@
 """reviews URL Configuration"""
 
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views, api_views
+
+router = DefaultRouter()
+router.register(r"books", api_views.BookViewSet)
+router.register(r"reviews", api_views.ReviewViewSet)
 
 urlpatterns = [
     path("", views.welcome_view, name="welcome_view"),
@@ -14,6 +18,6 @@ urlpatterns = [
     path("books/<int:book_id>/review/new", views.review_edit, name="review_create"),
     path("books/<int:book_id>/media/", views.book_media, name="book_media"),
     path("api/first_api_view/", api_views.first_api_view),
-    path("api/all_books/", api_views.AllBooks.as_view(), name="all_books"),
+    path("api/", include((router.urls, "api"))),
     path("api/contributors/", api_views.ContributorView.as_view(), name="contributors")
 ]
