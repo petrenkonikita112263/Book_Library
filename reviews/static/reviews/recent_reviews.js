f7f7bb346dcfd63891d74772aa8ff204e824164a
Nikita Petrenko <petrenkonikserg@gmail.com>
2022-03-02 18:46:10 +0200

    Implement render method.

--- reviews/static/reviews/recent_reviews.js
+++ reviews/static/reviews/recent_reviews.js
@@ -3,4 +3,22 @@ class ReviewDisplay extends React.Component {
         super(props);
         this.state = {review: props.review};
     }
+
+    render() {
+        const review = this.state.review;
+        return <div className="col mb-4">
+            <div className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{review.book}
+                        <strong>({review.rating})</strong>
+                    </h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{review.creator.email}</h6>
+                    <p className="card-text">{review.content}</p>
+                </div>
+                <div className="card-footer">
+                    <a href={'/books/' + review.book_id + '/'} className="card-link">View Book</a>
+                </div>
+            </div>
+        </div>;
+    }
 }
